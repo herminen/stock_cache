bcf15a1667a46950e0eaa8033c5f76e1dcdfbe2d
herminen <herminen@163.com>
2020-06-04 19:01:24 +0800

    init

---
+++ src/main/java/com/lh/stock/stockcache/StockCacheApplication.java
@@ -0,0 +1,13 @@
+package com.lh.stock.stockcache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockCacheApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockCacheApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lh/stock/stockcache/StockCacheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lh.stock.stockcache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockCacheApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
