352b2b935871b13be43e68dc240adcaeff1ac5d6
herminen <herminen@163.com>
2020-06-06 17:31:22 +0800

    add refresh cache data test unit

--- src/main/java/com/lh/stock/stockcache/kafka/Consumer.java
+++ src/main/java/com/lh/stock/stockcache/kafka/Consumer.java
@@ -3,15 +3,12 @@ package com.lh.stock.stockcache.kafka;
 import com.alibaba.fastjson.JSONObject;
 import com.lh.stock.stockcache.component.IFindFreshData;
 import com.lh.stock.stockcache.component.IFreshCache;
-import com.lh.stock.stockcache.domain.GoodsBaseInfo;
 import com.lh.stock.stockcache.domain.KafkaMsgContext;
-import com.lh.stock.stockcache.service.IGoodsInfoService;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -29,23 +26,19 @@ public class Consumer implements ApplicationContextAware {
 
     private ApplicationContext applicationContext;
 
-    @Qualifier("freshGoodsBaseInfoCache")
-    @Autowired
-    private IFreshCache freshGoodsBaseInfoCache;
-
     @Override
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
         this.applicationContext = applicationContext;
     }
 
     @KafkaListener(id = "stock-cache-group1", topics={"${kafka.stock.topic}"}, properties={"'subscriptionType':'USER_ASSIGNED'"})
-    public void consume(String msg){
-        logger.info(String.format("$$ -> Consumed Message -> %s",msg));
-        if(StringUtils.isBlank(msg)){
+    public void consume(ConsumerRecord<Integer, String> record){
+        logger.info(String.format("$$ -> Consumed Message -> %s",record.value()));
+        if(StringUtils.isBlank(record.value())){
             return;
         }
         //解析消息
-        KafkaMsgContext msgContext = JSONObject.parseObject(msg, KafkaMsgContext.class);
+        KafkaMsgContext msgContext = JSONObject.parseObject(record.value(), KafkaMsgContext.class);
         //调用接口查询数据
         fetchFreshData(msgContext);
         //操作缓存
